8d88183cc0b2f37ba700d0140c40861201c670e3
zedany7@hotmail.com <lk0544282120>
2019-04-07 14:03:21 +0300

    first

--- main.py
+++ main.py
@@ -10,5 +10,19 @@ config = {
 firebase = pyrebase.initialize_app(config)
 
 db = firebase.database()
-all_users = db.child("cho").child("users").get()
+all_users = db.child("cho").child("users").get().val()
+panelist={}
+
+for user in all_users:
+    print(user)
+    panelist['BA-SEnd']=all_users[user]['BA-SEnd']
+    panelist['BA-SStart']=all_users[user]['BA-SStart']
+    panelist['MIS-SEnd']=all_users[user]['MIS-SEnd']
+    panelist['MIS-SStart']=all_users[user]['MIS-SStart']
+    panelist['MOS-SEnd']=all_users[user]['MOS-SEnd']
+    panelist['MOS-SStart']=all_users[user]['MOS-SStart']
+    panelist['SES-SStart']=all_users[user]['SES-SStart']
+    panelist['SES-SEnd']=1554307848588
+    m = list(filter(lambda x: all_users[user]['bvpData'][x]['timeStamp'] > float(panelist['BA-SStart']/1000) and all_users[user]['bvpData'][x]['timeStamp']<float(panelist['BA-SEnd']/1000), all_users[user]['bvpData']))
+
 x=5
