0bdc20c8060d0e8b2344ed73246a677cc0cd98c8
obaidaabu <aobaida.17@gmail.com>
2019-04-07 15:18:13 +0300

    init data

--- main.py
+++ main.py
@@ -33,20 +33,49 @@ def get_user_data(user):
   panelist['SES-SStart']=user['SES-SStart']
   panelist['SES-SEnd']= 1554307848588
 
-  ba_s_bvpdata = list(filter(lambda x: user['bvpData'][x]['timeStamp'] > float(panelist['BA-SStart']/1000) and user['bvpData'][x]['timeStamp']<float(panelist['BA-SEnd']/1000), user['bvpData']))
-  mis_s_bvpdata = list(filter(lambda x: user['bvpData'][x]['timeStamp'] > float(panelist['MIS-SStart']/1000) and user['bvpData'][x]['timeStamp']<float(panelist['MIS-SEnd']/1000), user['bvpData']))
-  mos_s_bvpdata = list(filter(lambda x: user['bvpData'][x]['timeStamp'] > float(panelist['MOS-SStart']/1000) and user['bvpData'][x]['timeStamp']<float(panelist['MOS-SEnd']/1000), user['bvpData']))
-  ses_s_bvpdata = list(filter(lambda x: user['bvpData'][x]['timeStamp'] > float(panelist['SES-SStart']/1000) and user['bvpData'][x]['timeStamp']<float(panelist['SES-SEnd']/1000), user['bvpData']))
+  ba_s_bvpdata = []
+  mis_s_bvpdata = []
+  mos_s_bvpdata = []
+  ses_s_bvpdata = []
 
-  ba_s_gsrdata = list(filter(lambda x: user['gsrData'][x]['timeStamp'] > float(panelist['BA-SStart']/1000) and user['gsrData'][x]['timeStamp']<float(panelist['BA-SEnd']/1000), user['gsrData']))
-  mis_s_gsrdata = list(filter(lambda x: user['gsrData'][x]['timeStamp'] > float(panelist['MIS-SStart']/1000) and user['gsrData'][x]['timeStamp']<float(panelist['MIS-SEnd']/1000), user['gsrData']))
-  mos_s_gsrdata = list(filter(lambda x: user['gsrData'][x]['timeStamp'] > float(panelist['MOS-SStart']/1000) and user['gsrData'][x]['timeStamp']<float(panelist['MOS-SEnd']/1000), user['gsrData']))
-  ses_s_gsrdata = list(filter(lambda x: user['gsrData'][x]['timeStamp'] > float(panelist['SES-SStart']/1000) and user['gsrData'][x]['timeStamp']<float(panelist['SES-SEnd']/1000), user['gsrData']))
+  for (k, v) in user['bvpData'].items():
+    if v['timeStamp'] > float(panelist['BA-SStart'] / 1000) and v['timeStamp'] < float(panelist['BA-SEnd'] / 1000):
+      ba_s_bvpdata.append(v)
+    if v['timeStamp'] > float(panelist['MIS-SStart'] / 1000) and v['timeStamp'] < float(panelist['MIS-SEnd'] / 1000):
+      mis_s_bvpdata.append(v)
+    if v['timeStamp'] > float(panelist['MOS-SStart'] / 1000) and v['timeStamp'] < float(panelist['MOS-SEnd'] / 1000):
+      mos_s_bvpdata.append(v)
+    if v['timeStamp'] > float(panelist['SES-SStart'] / 1000) and v['timeStamp'] < float(panelist['SES-SEnd'] / 1000):
+      mos_s_bvpdata.append(v)
 
-  ba_s_ibidata = list(filter(lambda x: user['ibiData'][x]['timeStamp'] > float(panelist['BA-SStart']/1000) and user['ibiData'][x]['timeStamp']<float(panelist['BA-SEnd']/1000), user['ibiData']))
-  mis_s_ibidata = list(filter(lambda x: user['ibiData'][x]['timeStamp'] > float(panelist['MIS-SStart']/1000) and user['ibiData'][x]['timeStamp']<float(panelist['MIS-SEnd']/1000), user['ibiData']))
-  mos_s_ibidata = list(filter(lambda x: user['ibiData'][x]['timeStamp'] > float(panelist['MOS-SStart']/1000) and user['ibiData'][x]['timeStamp']<float(panelist['MOS-SEnd']/1000), user['ibiData']))
-  ses_s_ibidata = list(filter(lambda x: user['ibiData'][x]['timeStamp'] > float(panelist['SES-SStart']/1000) and user['ibiData'][x]['timeStamp']<float(panelist['SES-SEnd']/1000), user['ibiData']))
+  ba_s_gsrdata = []
+  mis_s_gsrdata = []
+  mos_s_gsrdata = []
+  ses_s_gsrdata = []
+  for (k, v) in user['gsrData'].items():
+    if v['timeStamp'] > float(panelist['BA-SStart'] / 1000) and v['timeStamp'] < float(panelist['BA-SEnd'] / 1000):
+      ba_s_gsrdata.append(v)
+    if v['timeStamp'] > float(panelist['MIS-SStart'] / 1000) and v['timeStamp'] < float(panelist['MIS-SEnd'] / 1000):
+      mis_s_gsrdata.append(v)
+    if v['timeStamp'] > float(panelist['MOS-SStart'] / 1000) and v['timeStamp'] < float(panelist['MOS-SEnd'] / 1000):
+      mos_s_gsrdata.append(v)
+    if v['timeStamp'] > float(panelist['SES-SStart'] / 1000) and v['timeStamp'] < float(panelist['SES-SEnd'] / 1000):
+      ses_s_gsrdata.append(v)
+
+  ba_s_ibidata = []
+  mis_s_ibidata = []
+  mos_s_ibidata = []
+  ses_s_ibidata = []
+
+  for (k, v) in user['ibiData'].items():
+    if v['timeStamp'] > float(panelist['BA-SStart'] / 1000) and v['timeStamp'] < float(panelist['BA-SEnd'] / 1000):
+      ba_s_ibidata.append(v)
+    if v['timeStamp'] > float(panelist['MIS-SStart'] / 1000) and v['timeStamp'] < float(panelist['MIS-SEnd'] / 1000):
+      mis_s_ibidata.append(v)
+    if v['timeStamp'] > float(panelist['MOS-SStart'] / 1000) and v['timeStamp'] < float(panelist['MOS-SEnd'] / 1000):
+      mos_s_ibidata.append(v)
+    if v['timeStamp'] > float(panelist['SES-SStart'] / 1000) and v['timeStamp'] < float(panelist['SES-SEnd'] / 1000):
+      ses_s_ibidata.append(v)
 
 
   data = {
